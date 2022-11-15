4130b7f877753aafc23cdbcd6764753a589cec76
millooo94 <camillomusmeciunict@gmail.com>
2022-11-15 16:46:04 +0100

    add all

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+    el: '.container',
+    data: {
+        arrTodo: [
+            {
+                todo: 'Studiare',
+                status: false,
+            },
+            {
+                todo: 'Fare la spesa',
+                status: true,
+            },
+            {
+                todo: 'Cucinare',
+                status: false,
+            },
+        ],
+        newTodo: {
+            todo: '',
+            status: false
+        }
+    },
+    methods: {
+        removeTodo(index){
+            this.arrTodo.splice(index, 1)
+        },
+        addTodo(){
+            this.arrTodo.push(this.newTodo)
+            this.newTodo.todo = '';
+        },
+        changeStatus() {
+            if (this.arrTodo[status]) {
+                !this.arrTodo[status]
+            }
+
+        }
+    },
+})
+
+
